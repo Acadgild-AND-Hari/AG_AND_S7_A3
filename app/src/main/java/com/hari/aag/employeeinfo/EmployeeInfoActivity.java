48907beb00c5454740233ba7af32863da666e4eb
harinivaskumar <harinivaskumar@live.com>
2016-12-23 03:27:36 +0530

    Code Commit
    Initial code commit

---
+++ app/src/main/java/com/hari/aag/employeeinfo/EmployeeInfoActivity.java
@@ -0,0 +1,13 @@
+package com.hari.aag.employeeinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmployeeInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employee_info);
+    }
+}
